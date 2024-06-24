06568b29aa17a3eef6cf19ac9f2ec0c284815afd
Anton5005 <106040288+Anton5005@users.noreply.github.com>
2024-06-24 17:29:49 +0300

    add eat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # pip install pygame
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -17,6 +18,11 @@ dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption("Snake Game")
 
 
+def draw_snake(snake_block, snake_list):
+    for element in snake_list:
+        pygame.draw.rect(dis, black, [element[0], element[1], snake_block, snake_block])
+
+
 def game_loop():
     game_over = False
     x = dis_width / 2
@@ -24,6 +30,13 @@ def game_loop():
 
     x_change = 0
     y_change = 0
+
+    snake_block = 10
+    snake_list = []
+    lenght_of_snake = 1
+
+    food_x = round(random.randrange(0, dis_width - snake_block) / 10.0)*10.0
+    food_y = round(random.randrange(0, dis_height - snake_block) / 10.0)*10.0
 
     while not game_over:
         for event in pygame.event.get():
@@ -43,12 +56,29 @@ def game_loop():
                     x_change = 0
                     y_change = -10
 
+        if x >= dis_width or x < 0 or y >= dis_height or y < 0:
+            game_over = True
+
         x += x_change
         y += y_change
-
         dis.fill(blue)
-        pygame.draw.rect(dis, black, [x, y, 10, 10])
+        pygame.draw.rect(dis, green, [food_x, food_y, snake_block, snake_block])
+
+        snake_head = []
+        snake_head.append(x)
+        snake_head.append(y)
+        snake_list.append(snake_head)
+
+        if len(snake_list) > lenght_of_snake:
+            del snake_list[0]
+
+        draw_snake(snake_block, snake_list)
         pygame.display.update()
+
+        if x == food_x and y == food_y:
+            food_x = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            food_y = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            lenght_of_snake += 1
 
         time.sleep(0.1)
 
