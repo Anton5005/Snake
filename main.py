834a29802fbdd984a91797cd269ef1b708b46918
Anton5005 <106040288+Anton5005@users.noreply.github.com>
2024-06-24 16:39:40 +0300

    go snake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# pip install pygame
 import pygame
+import time
 
 pygame.init()
 
@@ -17,14 +19,41 @@ pygame.display.set_caption("Snake Game")
 
 def game_loop():
     game_over = False
-    x = dis_width
-    y = dis_height
+    x = dis_width / 2
+    y = dis_height / 2
 
     x_change = 0
     y_change = 0
 
     while not game_over:
-#
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -10
+                    y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 10
+                    y_change = 0
+                elif event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = 10
+                elif event.key == pygame.K_UP:
+                    x_change = 0
+                    y_change = -10
+
+        x += x_change
+        y += y_change
+
+        dis.fill(blue)
+        pygame.draw.rect(dis, black, [x, y, 10, 10])
+        pygame.display.update()
+
+        time.sleep(0.1)
+
+    pygame.quit()
+    quit()
 
 
 game_loop()
